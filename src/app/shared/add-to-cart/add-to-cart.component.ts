7f8338136045e4348760ea5258234d76e73e1205
Edward <edwardlinton35@gmail.com>
2023-07-16 12:01:36 +0100

    feat: integrating pricing

--- src/app/shared/add-to-cart/add-to-cart.component.ts
+++ src/app/shared/add-to-cart/add-to-cart.component.ts
@@ -1,7 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
+import { forkJoin } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -12,14 +14,69 @@ import { environment } from 'src/environments/environment';
 export class AddToCartComponent {
   @Input() image: any;
   @Output() addToCartEvent = new EventEmitter<any>();
+  materials: any = [];
+  sizes: any = [];
+  prices: any = [];
+
   optionsForm = new FormGroup({
-    size: new FormControl('Small', [Validators.required]),
-    material: new FormControl('Canvas', [Validators.required])
+    size: new FormControl(null, [Validators.required]),
+    material: new FormControl(null, [Validators.required])
   })
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.getPrices();
+  }
+
+  getPrices() {
+    forkJoin([
+      this.http.get<{data: any[]}>(`${environment.apiUrl}items/materials?limit=-1`), 
+      this.http.get<{data: any[]}>(`${environment.apiUrl}items/sizes?limit=-1`), 
+      this.http.get<{data: any[]}>(`${environment.apiUrl}items/prices?limit=-1`)
+    ]).subscribe(([materials, sizes, prices]) => {
+      this.materials = materials.data;
+      this.sizes = sizes.data;
+      this.prices = prices.data;
+    })
+  }
 
   onAdd() {
     this.addToCartEvent.emit();
+  }
+
+  capitaliseFirstLetter(str: string) {
+   const splitStr = str.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   return splitStr.join(' '); 
+  }
+
+  getSizes() {
+    const sizes = this.prices.filter((p: any) => +p.material === +this.optionsForm.controls.material.value!).map((p: any) => p.size);
+    return this.sizes.filter((s: any) => sizes.includes(s.id));
+  }
+
+  getPrice(size: number) {
+    return this.prices.filter((p: any) => +p.size === +size && +p.material === +this.optionsForm.controls.material.value!)[0]?.price;
+  }
+
+  getImageTitle(image: any) {
+    if (!this.optionsForm.controls.material.value) {
+      return '';
+    }
+    return image.title + ' ' + this.capitaliseFirstLetter(this.materials.filter((m: any) => +m.id === +this.optionsForm.controls.material.value!)[0].material);
+  }
+
+  getSizesForSnipcart() {
+    const sizes = this.getSizes();
+    const sizeArray = [];
+    for (const size of sizes) {
+        sizeArray.push(`${size.size}[+${this.getPrice(size.id)}]`)
+    }
+    return sizeArray.join('|');
+  }
+
+  getCurrentSize() {
+    return this.sizes.filter((s: any) => +s.id === +this.optionsForm.controls.size.value!).size;
   }
 }
